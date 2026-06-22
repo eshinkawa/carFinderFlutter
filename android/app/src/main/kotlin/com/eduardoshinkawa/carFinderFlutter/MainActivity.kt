bc90ba563384f367244a5f2b8efe058a003f3117
Sparkweb-Eng <devmailsparkweb@gmail.com>
2026-06-22 01:48:06 -0300

    build: upgrade Flutter SDK, dependencies, and native platform configs
    - Upgrade Dart SDK constraint to >=3.0.0
    - Update all packages to null-safe versions
    - Android: AGP 8.1.0, Kotlin 1.9.22, Gradle 8.3, SDK 34, namespace
    - iOS: set deployment target 13.0, modern Podfile
    - Add native splash screen assets (Android drawable + iOS LaunchImage)
    - Fix MainActivity package to match namespace

--- android/app/src/main/kotlin/com/eduardoshinkawa/carFinderFlutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.eduardoshinkawa.carFinderFlutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/eduardoshinkawa/carfinder/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.eduardoshinkawa.carfinder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
